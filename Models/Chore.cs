4cfb0bbf7c1a8ec8c09a6a6a21cc12d4a782b777
Andrew Rady <andrew.arsoftware@gmail.com>
2021-01-17 17:24:36 -0700

    adds required and and limits to chore model

--- Models/Chore.cs
+++ Models/Chore.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace homeChores.Models
 {
     public class Chore
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+        [Required]
+        [Range(0, 10)]
         public int Level { get; set; }
     }
 }
